86caf3a34f868e0d2f5bd50602b54e47e8b9b836
Victor Silva <silvavictorleandro@outlook.com>
2024-02-26 14:01:32 -0300

    refatorando o projeto inicial para usar funções

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Este projeto foi desenvolvido durante um desafio no curso de Fundamentos de Python, na plataforma DIO.
 
-O objetivo foi implementar três operações essenciais em um sistema bancário: depósito, saque e extrato.
+Na primeira versão do projeto, o objetivo foi implementar três operações essenciais em um sistema bancário: depósito, saque e extrato.
 
 Algumas condições deviam ser impostas para determinadas situações:
 
@@ -10,3 +10,5 @@ Algumas condições deviam ser impostas para determinadas situações:
 - Limite de R$ 500,00 por saque realizado;
 - Permissão de até 3 saques diários;
 - Exibir no extrato todas as operações realizadas.
+
+Na segunda versão, o objetivo foi refatorar as operações, dessa vez utilizando funções, e adicionar as funções de criar usuários, criar contas e listar tudo.
--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,63 +1,108 @@
-menu = """
-Informe a opção desejada:
-
-    [1] Depositar
-    [2] Sacar
-    [3] Extrato
-    [0] Sair
-
-"""
-
-saldo = 0
-numero_saques = 0
-LIMITE_QUANTIDADE_SAQUES = 3
-LIMITE_VALOR_SAQUE = 500
-extrato = ""
+import textwrap
 
 
-while True:
-    pergunta_inicial = int(input(f"{menu}"))
+def menu():
+    menu = """\n
+    ============ MENU ============
+    [1]\tDepositar
+    [2]\tSacar
+    [3]\tExtrato
+    [4]\tNovo usuário
+    [5]\tNova conta
+    [6]\tListar contas
+    [0]\tSair
+    => """
+    return input(textwrap.dedent(menu))
 
-    if pergunta_inicial == 1:
-        valor_deposito = float(input("\nDigite o valor que deseja depositar: \n\n"))
-        if valor_deposito > 0:
-            saldo += valor_deposito
-            extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
-            print(f"\nDepósito de R$ {valor_deposito:.2f} realizado com sucesso!")
-            print("\nDeseja mais alguma coisa?")
-        else:
-            print("\nValor inválido para depósito! Digite novamente:\n")
 
-    elif pergunta_inicial == 2:
-        valor_saque = float(input("Digite o valor que deseja sacar: "))
+def depositar(saldo, valor_deposito, extrato, /):
 
-        if valor_saque <= LIMITE_VALOR_SAQUE:
-            if valor_saque <= saldo:
-                if numero_saques < LIMITE_QUANTIDADE_SAQUES:
-                    numero_saques += 1
-                    saldo -= valor_saque
-                    extrato += f"Saque: R$ {valor_saque:.2f}\n"
-                    print(f"\nSaque de R$ {valor_saque:.2f} realizado com sucesso!")
-                    print("\nDeseja mais alguma coisa?")
-                else:
-                    print("\nLimite de saques diário atingido.")
-                    print("\nSão permitidos apenas 3 saques diários.")
-                    print("\nDeseja mais alguma coisa?")                    
-            else:
-                print("\nSaldo insuficiente! Verifique o saldo atual e tente novamente")
-        else:
-            print("\nValor inválido! Só é possível saque de até R$ 500,00")
-
-    elif pergunta_inicial == 3:
-        print("============== EXTRATO ==============")
-        print(f"{extrato}")
-        print(f"{saldo:.2f}")
-        print("=====================================")
-        print("\nDeseja mais alguma coisa?")
-
-    elif pergunta_inicial == 0:
-        print("\nAgradecemos o contato!")
-        break
+    if valor_deposito > 0:
+        saldo += valor_deposito
+        extrato += f"Depósito:\tR$ {valor_deposito:.2f}\n"
+        print(f"\n=== Depósito de R$ {valor_deposito:.2f} realizado com sucesso! ===")
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("\n@@@ Valor inválido para depósito! @@@\n")
+
+    return saldo, extrato
+
+
+def sacar(*, saldo, valor_saque, extrato, limite, numero_saques, limite_quantidade_saques):
+
+    excedeu_saldo = valor_saque > saldo
+    excedeu_limite = valor_saque > limite
+    excedeu_saques = numero_saques >= limite_quantidade_saques
+
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente! @@@")
+
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite de R$ 500,00 @@@")
+
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques excedidos! @@@")
+        
+    elif valor_saque <= saldo:
+        saldo -= valor_saque
+        extrato += f"Saque:\t\tR$ {valor_saque:.2f}\n"
+        numero_saques += 1
+        print(f"\nSaque de R$ {valor_saque:.2f} realizado com sucesso!")
+
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido! @@@")
+    
+    return saldo, extrato
+
+
+def exibir_extratos(saldo, /, *, extrato):
+    print("\n========== EXTRATO ==========")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    print("=============================")
+
+
+def main():
+    LIMITE_QUANTIDADE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+    numero_conta = 0
+
+    while True:
+        pergunta_inicial = menu()
+
+        if pergunta_inicial == "1":
+            valor_deposito = float(input("\nDigite o valor que deseja depositar: \n\n"))
+
+            saldo, extrato = depositar(saldo, valor_deposito, extrato)            
+
+        elif pergunta_inicial == "2":
+            valor_saque = float(input("Digite o valor que deseja sacar: "))
+
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor_saque=valor_saque,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_quantidade_saques=LIMITE_QUANTIDADE_SAQUES
+            )            
+
+        elif pergunta_inicial == "3":
+            exibir_extratos(saldo, extrato=extrato)
+
+        elif pergunta_inicial == "0":
+            print("\nAgradecemos o contato!")
+            break
+
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+main()
